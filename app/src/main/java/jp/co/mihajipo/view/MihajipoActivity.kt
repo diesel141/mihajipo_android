f98cc308dd4b3d5b581244b75068dc34596e0de6
141diesel <141diesel@gmail.com>
2021-10-21 09:45:29 +0900

    プロジェクト新規作成

---
+++ app/src/main/java/jp/co/mihajipo/view/MihajipoActivity.kt
@@ -0,0 +1,12 @@
+package jp.co.mihajipo.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import jp.co.mihajipo.R
+
+class MihajipoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mihajipo)
+    }
+}
