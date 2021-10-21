c93e60f04966b01973d8a9c87735f23c0e3755cb
sirensan <hosinahikaru@gmail.com>
2021-10-21 19:08:05 +0900

    タイマーの枠のみ作成

--- app/src/main/java/jp/co/mihajipo/utility/TimerUtility.kt
+++ app/src/main/java/jp/co/mihajipo/utility/TimerUtility.kt
@@ -1,2 +1,25 @@
 package jp.co.mihajipo.utility
 
+import kotlinx.coroutines.*
+import java.util.concurrent.atomic.AtomicBoolean
+
+object Timer {
+    private val coroutineScope: CoroutineScope = GlobalScope
+    private var countDownJob: Job? = null
+    private var time = 0;
+    private val isActive = AtomicBoolean(true)
+
+    fun countDownStart() {
+        /*if (countDownJob != null) { return }*/
+        countDownJob = coroutineScope.launch {
+            while(isActive) {
+                delay(1000)
+                ++time
+            }
+        }
+    }
+
+    fun countDownStop() {
+        //countDownJob?.cancel()
+    }
+}
