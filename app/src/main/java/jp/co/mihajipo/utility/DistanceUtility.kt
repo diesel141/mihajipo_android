bb638190dfd8a99fb00b1155c18edd8e7d77408b
testJcSin <rrrtestjc@gmail.com>
2022-04-13 15:05:51 +0900

    2地点間の距離算出メソッド作成

--- app/src/main/java/jp/co/mihajipo/utility/DistanceUtility.kt
+++ app/src/main/java/jp/co/mihajipo/utility/DistanceUtility.kt
@@ -1,2 +1,29 @@
 package jp.co.mihajipo.utility
 
+import android.location.Location
+import android.util.Log
+
+/**
+ * 距離共通処理を提供するクラス
+ */
+object DistanceUtility {
+
+    /**
+     * 2地点間の距離を算出する
+     * @args tmpFastLocation 始点location
+     * @args tmpNextLocation 終点location
+     * @return String 2地点間の距離（m）
+     */
+    fun calcBetweenTwoPoints(tmpFastLocation: Location, tmpNextLocation: Location): String {
+        val results = FloatArray(1)
+        Location.distanceBetween(
+            tmpFastLocation.latitude,
+            tmpFastLocation.longitude,
+            tmpNextLocation.latitude,
+            tmpNextLocation.longitude,
+            results
+        );
+        Log.d("debug", "距離確認：" + (results[0]/100).toString())
+        return (results[0]/100).toString()
+    }
+}
